da2824e5c1979bc1141426e6370c970d8d3e772d
kiran alex ch <kiranalexc@gmail.com>
2023-04-07 07:02:34 +0000

    demo routes added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
+import { Route,Routes,Navigate, Link } from 'react-router-dom';
+import Login from './Login';
 
 function App() {
   return (
     <div className="App">
       <h1>hello kiran</h1>
+      <Routes>
+      <Route path="/login" element={<Login/>} />
+      <Route path='/' element={ <Navigate to="/login"  replace/>}/>
+      </Routes>
     </div>
   );
 }
