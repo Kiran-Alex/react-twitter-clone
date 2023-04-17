b828db687a40d2f414f44f383cbb533835793f6e
kiran alex ch <kiranalexc@gmail.com>
2023-04-17 05:11:31 +0000

    created the container and setted the icons

--- src/Tweetbox.js
+++ src/Tweetbox.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import Home from './Home';
 import "./styles/tweetbox.css"
+import CloseIcon from '@mui/icons-material/Close';
 
 function Tweetbox() {
   const [text, setText] = useState("");
@@ -18,11 +19,23 @@ function Tweetbox() {
   return (
     <React.Fragment>
       <div className='roi'>
-      <div className='tbbg'>
-        
+        <div className='tbbg'>
+
+          <div className='tbcontainer'>
+            <div className='tbc-header'>
+              <div className='tbc-h-img'>
+                  <CloseIcon sx={{color: "white"}}/>
+              </div>
+              <span >Unsent Tweets</span>
+            </div>
+            <div className='tbc-content'>
+            </div>
+            <div className='tbc-footer'></div>
+          </div>
+
         </div>
         <Home />
-       
+
       </div>
     </React.Fragment>
   )
