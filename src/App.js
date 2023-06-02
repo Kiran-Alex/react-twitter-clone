9d0a1076ee011c7c22fd0235ccca3843c20185f6
kiran alex ch <kiranalexc@gmail.com>
2023-06-02 10:53:29 +0000

    bug fix

--- src/App.js
+++ src/App.js
@@ -9,7 +9,6 @@ import Explore from './Explore';
 import Notification from './Notification';
 import Bookmarks from './Bookmarks';
 import Messages from './Messages';
-import MessageCompose from './MessageCompose';
 
 
 function App() {
@@ -25,7 +24,6 @@ function App() {
       <Route path='/notification' element={<Notification/>}/>
       <Route path='/Bookmarks' element={<Bookmarks/>}/>
       <Route path='/Messages' element={<Messages/>}/>
-      <Route path='/Messages/Compose' element={<MessageCompose/>}/>
       </Routes>
     </React.Fragment>
   );
